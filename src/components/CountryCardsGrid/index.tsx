aed56790f5166e6e96fce4511ad37449e348d5a0
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-02 18:07:11 +0100

    refactor: migrate countries grid to tailwindcss

--- src/components/CountryCardsGrid/index.tsx
+++ src/components/CountryCardsGrid/index.tsx
@@ -5,9 +5,7 @@ import { Card } from "@/components/ui/Card";
 import { Modal } from "@/components/ui/Modal";
 import { generateCountriesGrid, type Country } from "@/helpers/country";
 import { GameStage } from "@/helpers/gameState";
-import { getEmojiForCountry } from "@/helpers/util";
-import cardStyles from "./CountryCard.module.scss";
-import styles from "./CountryCardsGrid.module.scss";
+import { cn, getEmojiForCountry } from "@/helpers/util";
 
 export const CountryCardsGrid = ({ countries }: { countries: Country[] }) => {
 	const {
@@ -20,7 +18,7 @@ export const CountryCardsGrid = ({ countries }: { countries: Country[] }) => {
 	}, [mainCountry, countries]);
 
 	return (
-		<section className={styles["neighbours"]}>
+		<section className="relative grid auto-cols-fr grid-cols-2 place-items-center gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
 			{stage !== GameStage.OnGoing && (
 				<Modal header="Game over">
 					{stage === GameStage.Won
@@ -44,10 +42,6 @@ const CountryCard = ({ country }: { country: Country }) => {
 	const [cardState, setCardState] = useState<CardStateOptions | null>(null);
 
 	const isCardCorrect = mainCountry?.borders.includes(country.cca3);
-	const showNotFound =
-		stage !== GameStage.OnGoing &&
-		isCardCorrect &&
-		cardState !== CardStateOptions.Correct;
 
 	const handleCardClick = () => {
 		if (cardState || stage !== GameStage.OnGoing) return;
@@ -59,15 +53,26 @@ const CountryCard = ({ country }: { country: Country }) => {
 
 	return (
 		<Card
-			className={`${cardStyles["country-card"] ?? ""} ${
-				cardState ? (cardStyles[cardState] ?? "") : ""
-			} ${showNotFound ? (cardStyles["not-found"] ?? "") : ""}`}
+			className={cn(
+				"flex h-52 w-40 cursor-pointer flex-col items-center justify-evenly px-2 text-center transition-colors duration-500 ease-out hover:bg-orange-100",
+				{
+					"bg-neutral-200": cardState,
+					"border-4 border-emerald-600": cardState === CardStateOptions.Correct,
+					"border-4 border-red-600": cardState === CardStateOptions.Incorrect,
+					"border-4 border-amber-500":
+						stage !== GameStage.OnGoing &&
+						isCardCorrect &&
+						cardState !== CardStateOptions.Correct,
+				},
+			)}
 			onClick={handleCardClick}
 		>
-			<div className={cardStyles["country-card__icon"]}>
+			<div className="w-full font-[TwemojiMozilla] text-7xl">
 				{getEmojiForCountry(country.cca2)}
 			</div>
-			<p className={cardStyles["country-card__text"]}>{country.name.common}</p>
+			<p className="max-w-full truncate text-xl text-wrap">
+				{country.name.common}
+			</p>
 		</Card>
 	);
 };
